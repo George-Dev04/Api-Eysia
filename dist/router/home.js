3f89dbc55a64d7041657b2339d37096e666297bd
George-Dev04 <georgerc04@gmail.com>
2026-08-27 00:22:21 -0300

    Remover dist do versionamento

--- dist/router/home.js
+++
@@ -1,6 +0,0 @@
-import { Elysia } from "elysia";
-export const homeRoutes = (app) => app.get("/", () => ({
-    message: "Bem, vindo a minha api",
-    status: 200
-}));
-//# sourceMappingURL=home.js.map
